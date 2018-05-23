c392340023aaf60266e35a74b7c0436b081c76f2
James Edington <james.edington@peraton.com>
2018-05-23 17:31:15 -0500

    Total rewrite including implementing a fifo256 class

--- main.ino
+++ main.ino
@@ -1,3 +1,6 @@
+enum system_state {BASIC, CMD_WAITING, STATE_RESET=0xFF};
+uint8_t system_state=BASIC;
+
 const uint8_t WHITE_LED = A5;
 const uint8_t CLEAR_LED = A4;
 const uint8_t BLUE_LED = A3;
@@ -12,18 +15,32 @@ const uint8_t BITMASK_1BIT[] = {
  0b00010000,
  0b00100000,
  0b01000000,
- 0b10000000,
+ 0b10000000
 };
 
-uint8_t incomingFifo[256];
-uint8_t fifoWriteIndex=0;
-uint8_t fifoReadIndex=0;
+struct fifo256 {
+ uint8_t fifo256::availr();
+ uint8_t fifo256::availw();
+ void fifo256::write(uint8_t);
+ uint8_t fifo256::read();
+ 
+ uint8_t fifo256::_read_head;
+ uint8_t fifo256::_write_head;
+ uint8_t fifo256::_data[256];
+};
 
-uint8_t drainctr;
-uint8_t drain_hw(uint8_t);
-uint8_t fifo_avail();
-uint8_t fifo_pop_safe();
-void pin_batch(uint8_t);
+fifo256 incoming;
+fifo256 cmd;
+
+uint8_t drain_hw(uint8_t, fifo256 buf=incoming);
+uint8_t drainctr=0x01;
+uint8_t cmd_read_2=0;
+void process_byte(uint8_t);
+void queueCMD(uint8_t);
+void setPins(uint8_t);
+
+bool should_reset();
+void(* RESET)(void) = 0; // stolen from Instructables lol but seems legit
 
 // the setup routine runs once when you press reset:
 void setup(){ 
@@ -31,35 +48,59 @@ void setup(){
 	pinMode(RED_LED, OUTPUT);
 	pinMode(YELLOW_LED, OUTPUT);
 	pinMode(GREEN_LED, OUTPUT);
-//	pinMode(BLUE_LED, OUTPUT);
+	pinMode(BLUE_LED, OUTPUT);
 //	pinMode(CLEAR_LED, OUTPUT);
 	pinMode(WHITE_LED, OUTPUT);
 	
 	// P.O.S.T.
 	drainctr=0x01;
 	do {
-		// Iterates from 1 to 255, inclusive,
-		// then 0
+		// Iterates from 0x01 to 0xFF, inclusive,
+		// then finishes with 0x00
 		Serial.print(drainctr, HEX);
-		pin_batch(drainctr);
+		process_byte(drainctr);
 		Serial.println("");
 	} while (drainctr++);
 }
 
-void pin_batch(uint8_t state){
-	// This is ONLY MEANT TO BE USED
-	// IF THE HIGH BIT IS NOT SET
-	if (state & BITMASK_1BIT[7])
-		return;
-	// for now, just exit inertly if
-	// passed a byte with high-bit=1
+// the loop routine runs over and over again forever:
+void loop(){
+	if (system_state==BASIC){
+		//1. Pop the FIFO into the LEDs
+		//   (Waiting for a byte from HW if needed)
+		while (!Serial.available())
+			delay(100);
+		incoming.write(Serial.read());
+		
+		process_byte(incoming.read());
+		
+		//2. FULLY DRAIN (our) fifo into the LEDs
+		while (incoming.availr())
+			process_byte(incoming.read());
+	
+		//3. Drain up to 16 bytes from the HW buffer
+		drain_hw(16);
+	}
+}
 
+void process_byte(uint8_t byte){
+	if (byte & BITMASK_1BIT[7]){
+		//High bit is SET/1
+		return queueCMD(byte);
+	} else {
+		//High bit is UNSET/0
+		return setPins(byte);
+	}
+}
+
+void setPins(uint8_t pinStateByte){
 	// http://forum.arduino.cc/index.php?topic=111711.msg839464#msg839464
 	// digitalWrite presumes its second argument as bool-castable
-	digitalWrite(WHITE_LED, state & BITMASK_1BIT[0] );
-	digitalWrite(RED_LED  , state & BITMASK_1BIT[3] );
-	digitalWrite(YELLOW_LED,state & BITMASK_1BIT[2] );
-	digitalWrite(GREEN_LED, state & BITMASK_1BIT[1] );
+	digitalWrite(WHITE_LED, pinStateByte & BITMASK_1BIT[0] );
+	digitalWrite(RED_LED  , pinStateByte & BITMASK_1BIT[3] );
+	digitalWrite(YELLOW_LED,pinStateByte & BITMASK_1BIT[2] );
+	digitalWrite(GREEN_LED, pinStateByte & BITMASK_1BIT[1] );
+	digitalWrite(BLUE_LED,  pinStateByte & BITMASK_1BIT[4] );
 	
 	// Delay ~1/32 of a second for digitalWrite()s to propagate
 	delay(31);
@@ -67,22 +108,12 @@ void pin_batch(uint8_t state){
 	// This will probably require a program rewrite however
 }
 
-// the loop routine runs over and over again forever:
-void loop(){
-	//1. Pop the FIFO into the LEDs
-	//   (Waiting for a byte if needed)
-	pin_batch(fifo_pop_safe());
-	
-	//2. FULLY DRAIN our fifo into the LEDs
-	while (fifo_avail())
-		pin_batch(fifo_pop_safe());
-
-	//3. Drain up to 16 bytes from the HW buffer
-	drain_hw(16);
+void queueCMD(uint8_t byte){
+	if (cmd.availw())
+		cmd.write(byte);
 }
-//	(4. repeat steps 1-3)
 
-uint8_t drain_hw(uint8_t qty){
+uint8_t drain_hw(uint8_t qty=0, fifo256 buf){
 	// As long as ALL OF:
 	//  a) We have unfilled (or unlimited) drain request
 	//  b) There is stuff available to fill it
@@ -96,29 +127,38 @@ uint8_t drain_hw(uint8_t qty){
 	&&
 	 Serial.available()
 	&&
-	 fifoWriteIndex+1 != fifoReadIndex
+	 buf.availw()
 	){
-		incomingFifo[++fifoWriteIndex] = Serial.read();
+		buf.write(Serial.read());
 		drainctr++;
 	}
 	
 	return drainctr;
 }
 
-uint8_t fifo_avail(){
-	return fifoWriteIndex - fifoReadIndex;
+uint8_t fifo256::availr(){
+	return this->_write_head - this->_read_head;
 }
 
-uint8_t fifo_pop_raw(){
-	return incomingFifo[++fifoReadIndex];
+uint8_t fifo256::availw(){
+	return this->_read_head - this->_write_head;
 }
 
-uint8_t fifo_pop_safe(){
-	if (fifo_avail())
-		return fifo_pop_raw();
-	else
-		while (not drain_hw(1))
-		 delay(100);
-	// This code SHOULD NOT execute until drain_hw returns True
-	return fifo_pop_raw();
+uint8_t fifo256::read(){
+	return this->_data[++this->_read_head];
+}
+
+void fifo256::write(uint8_t byte){
+	this->_data[++this->_write_head]=byte;
+}
+
+bool should_reset(){
+	cmd_read_2=0;
+	do {	//STRCMP is for WUSSES
+		if (cmd._data[cmd_read_2] == 0x52) // "R"
+		if (cmd._data[cmd_read_2] == 0x53) // "S"
+		if (cmd._data[cmd_read_2] == 0x0a) // "\n"
+		return true;
+	} while (++cmd_read_2);
+	return false;
 }
