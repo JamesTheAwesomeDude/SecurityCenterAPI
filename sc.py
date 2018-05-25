77b3088fd5e12944d0778cab97230fd5b7c084c9
James Edington <james.edington@peraton.com>
2018-05-25 13:10:30 -0500

    Refactored to a proper Class-based model
    The SecurityCenterAPI class has some work yet remaining,
    though this looks like it's going to be MUCH more sustainable

--- sc.py
+++ sc.py
@@ -7,7 +7,132 @@ import urllib3,json
 
 assert version_info >= (3,)
 
-VERSION=(0,0,3)
+VERSION=(0,0,5)
+
+class SecurityCenterAPI:
+	def __init__(self,
+	     hostname,
+	     u_p=(None, None),
+	     port=443,
+	     protocol='https',
+	     endpoint='/rest',
+	     
+	     extra_http_kwargs={
+	      'assert_hostname': False #WORKAROUND Re:Jeff
+	     },
+	     extra_headers={
+	      'Host':"10.10.10.8"      #WORKAROUND Re:Jeff
+	     },
+	     
+	     # Overridable via the "extra_..", so should almost never be used:
+	     _headers={
+	      'User-Agent': 'ArduinoMonitor / {}'.format('.'.join([str(V) for V in VERSION]))
+	     },
+	     _http_kwargs={
+	      'ca_certs': "/opt/sc/support/conf/TenableCA.crt",
+	      'cert_reqs': 'CERT_REQUIRED' # idk what this does but the wiki said to put it when using custom CA
+	     },
+	     token_header_name='X-SecurityCenter',
+	     DEBUG=True
+	):
+		## __init__ STARTS HERE ##
+		
+		# Initialize the HTTP Connection Pool
+		self._http = urllib3.PoolManager(
+		 headers=dict(_headers, **extra_headers),
+		 **dict(_http_kwargs, **extra_http_kwargs)
+		)
+		
+		# Record the state of DEBUG mode
+		self._DEBUG=DEBUG
+		
+		# Initialize helper functions
+		self._r2u = lambda resource ,protocol=protocol,hostname=hostname,port=str(port),endpoint=endpoint,s=('://',':','/') : ''.join(
+		 (protocol,s[0],hostname,s[1],port,endpoint,s[2],resource)
+		)
+		self._t2h = lambda t,n=token_header_name: ({n: str(t)} if t else {})
+		self._token = {'token': 0, 'Cookie': None}
+		self._token_headers = lambda token=self._token,token_header_name=token_header_name,: (
+		 dict(
+		  ({token_header_name: str(token['token'])} if token['token']  else {}),
+		  **(       {'Cookie': token['Cookie']}     if token['Cookie'] else {})
+		 )
+		)
+		
+		# Log in
+		if u_p[0]:
+			self.login(*u_p)
+		else:
+			print("WARNING: deferring login. You will have to .login(username,password)!")
+		
+		## __init__ ENDS HERE ##
+	
+	def _get_resource(self, resource, method='GET', headers={}, r2u=None, _req_kwargs={}):
+		r2u = (self._r2u if r2u is None else r2u)
+		
+		if self._DEBUG:
+			print("##FETCH##")
+			print("RESOURCE:   ", resource)
+			print("HEADERS:    ", headers)
+			print("_REQ_KWARGS:", _req_kwargs)
+			print("URL:        ", r2u(resource))
+		
+		r = self._http.request(
+		 method,
+		 r2u(resource),
+		 headers=headers,
+		 **_req_kwargs
+		)
+		
+		if r.status in [403,404,400]:
+			raise urllib3.exceptions.HTTPError(r)
+		return r
+		
+	def get(self, resource, method='GET', token=None, headers={}, _req_kwargs={}, _PROCESS=lambda r: (json.loads(r.data.decode())) ):
+		if token is None:
+			token=self._token
+		
+		r = self._get_resource(
+		 resource=resource,
+		 method=method,
+		 headers=dict(
+		  self._token_headers(),
+		  **headers
+		 ),
+		 _req_kwargs=_req_kwargs
+		)
+
+		if self._DEBUG:
+			print("JSON:       ", json.loads(r.data))
+		
+		return _PROCESS(r)
+
+	def login(self, username, password):
+		#TODO: Client Certificate support
+		# https://docs.tenable.com/sscv/api/System.html#SystemGET
+		
+		t_c = self.get(
+		 'token',
+		 method='POST',
+		 _req_kwargs={'fields':{
+		  'username': username,
+		  'password': password
+		 }},
+		 _PROCESS=lambda r: (json.loads(r.data.decode())['response']['token'], r.headers['Set-Cookie'])
+		)
+		if self._DEBUG:
+			print(t_c)
+		
+		self._token['token'] = t_c[0]
+		self._token['Cookie']= t_c[1]
+	
+	
+#	def __enter__(self):
+#		#TODO
+#	def __exit__(self):
+#		#TODO
+	
+	
 
 def __main__(argv=argv):
 	username=hostname=password=None
@@ -39,91 +164,20 @@ def __main__(argv=argv):
 	
 	print("Testing login...")
 	
-	sc=_init_sc(
-	 (username,password),
-	 hostname,
-	 port,
-	 protocol
+	SC=SecurityCenterAPI(
+	 u_p=(username,password),
+	 hostname=hostname,
+	 port=port,
+	 protocol=protocol
 	)
 	
-	if sc.token:
-		return 0 # for Success
-	else:
-		return 1 # for Error
+	print("Logged in! Token:", SC._token)
+	
+	print("Trying scanResult..")
+	R=SC.get("scanResult", DEBUG=True)
+	print("Status: \t"+ str(R.status))
+	print("Headers:\t"+str([h for h in R.headers if h.startswith('X')]) + "\t(startswith('X') only)")
 
-
-def init_sc(
-   u_p,
-   hostname,
-   port=443,
-   protocol='https',
-   endpoint="/rest"
-):
-	
-	http = urllib3.PoolManager(	
-	
-	headers= {
-	  'User-Agent':'ArduinoMonitor / {}'.format('.'.join([str(V) for V in VERSION]))
-	 },
-	 
-	 cert_reqs='CERT_REQUIRED',
-	 ca_certs="/opt/sc/support/conf/TenableCA.crt",
-	 
-	 # LEAVE THIS IN:
-	 assert_hostname=False
-	 # (at least until we can get Jeff
-	 #  to STAHP configuring SC+Nessus
-	 #   with non-loopback IPs...)
-	)
-	
-	http.headers['Host']="10.10.10.8" # (Likewise...)
-	
-	make_get_function = lambda token,url_base_elements=(protocol, '://', hostname, ':', port, endpoint):(
-	 lambda resource_name,method='GET',token=None,http=http,res2u=(  lambda resrcnm: ''.join(url_base_elements+(resrcnm,)) ),request_extra=([],{}):(
-	  http.request(
-	   method,
-	   res2u(resource_name),
-	   *request_extra[0],
-	   **request_extra[1]
-	  )
-	 )
-	)
-
-	r_login=make_get_function(token=None)(
-	 '/token',
-	 method='POST',
-	 request_extra=(
-	  [],
-	  {
-	   'fields':{
-	    'username': u_p[0],
-	    'password': u_p[1]
-	   }   
-	  }
-	 )
-	)
-
-	login_data=json.loads(r_login.data.decode())
-	
-	if 'error_code' in login_data and login_data['error_code']:
-		_raise_http_json(login_data)
-	
-	
-	token=login_data['response']['token']	
-	http.headers['X-SecurityCenter']=str(token)
-	
-	return namedtuple(
-		'SecurityCenterAPI',
-		[
-		 'http',
-		 'token',
-		 'get',
-		]
-	)(
-		http,
-		token,
-		make_get_function(token)
-	)
 
 def _raise_http_json(msg_dict):
 	raise urllib3.exceptions.HTTPError(
